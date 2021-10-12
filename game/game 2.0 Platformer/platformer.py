dbe12029a169be85c9e2524da9990006879e88e1
Lardorat <berestin.k@yandex.ru>
2021-10-12 21:42:21 +0500

    Платфоре уже с челиком, котрый двигается

--- game/game 2.0 Platformer/platformer.py
+++ game/game 2.0 Platformer/platformer.py
@@ -3,80 +3,136 @@ from pygame.locals import *
 
 pygame.init()
 
-screen_widht = 1000
-screen_heigtht = 1000
+clock = pygame.time.Clock()
+fps = 60
 
-screen = pygame.display.set_mode((screen_widht, screen_heigtht))
+
+screen_widht = 1000
+screen_height = 1000
+
+screen = pygame.display.set_mode((screen_widht, screen_height))
 pygame.display.set_caption('Platformer')
 
 tile_size = 50
+game_over = 0
 
 #скачиваем картинку
 bg_img = pygame.image.load('fon.png')
 
-def draw_grid():
-    for line in range(0,20):
-        pygame.draw.line(screen, (255, 255, 255), (0, line * tile_size), (screen_widht, line * tile_size))
-        pygame.draw.line(screen, (255, 255, 255), ( line * tile_size,0), (line * tile_size, screen_heigtht))
-
 class Player():
     def __init__(self, x, y ):
-        img = pygame.image.load('p1_walk09.png')
-        self.image = pygame.transform.scale(img, (40, 80))
+        self.images_right = []
+        self.images_left = []
+        self.index = 0
+        self.counter = 0
+        for num in range(1, 12):
+            img_right = pygame.image.load(f'p1_walk{num}.png')
+            img_right = pygame.transform.scale(img_right, (45, 80))
+            img_left = pygame.transform.flip(img_right, True, False)
+            self.images_right.append(img_right)
+            self.images_left.append(img_left)
+        self.dead_image = pygame.image.load('flyFly2.png')
+        self.image = self.images_right[self.index]
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
+        self.width = self.image.get_width()
+        self.height = self.image.get_height()
         self.vel_y = 0
         self.jumped = False
+        self.direction = 0
 
-
-    def update(self):
+    def update(self, game_over):
         dx = 0
         dy = 0
-        # Получаем ключ
-        key = pygame.key.get_pressed()
-        if key[pygame.K_SPACE] and self.jumped == False:
-            self.vel_y = -15
-            self.jumped = True
-        if key[pygame.K_SPACE] == False:
-            self.jumped = False
-        if key[pygame.K_LEFT]:
-            dx -= 5
-        if key[pygame.K_RIGHT]:
-            dx += 5
+        walk_cooldown = 6
+
+        if game_over == 0:
+            # Получаем ключ
+            key = pygame.key.get_pressed()
+            if key[pygame.K_SPACE] and self.jumped == False:
+                self.vel_y = -15
+                self.jumped = True
+            if key[pygame.K_SPACE] == False:
+                self.jumped = False
+            if key[pygame.K_LEFT]:
+                dx -= 5
+                self.counter += 1
+                self.direction = -1
+            if key[pygame.K_RIGHT]:
+                dx += 5
+                self.counter += 1
+                self.direction = 1
+            if key[pygame.K_LEFT] == False and key[pygame.K_RIGHT] == False:
+                self.counter = 0
+                self.index = 0
+                if self.direction == 1:
+                    self.image = self.images_right[self.index]
+                if self.direction == -1:
+                    self.image = self.images_left[self.index]
+
+            # Создаем онимацию для персонжа
+            if self.counter > walk_cooldown:
+                self.counter = 0
+                self.index += 1
+                if self.index >= len(self.images_right):
+                    self.index = 0
+                if self.direction == 1:
+                    self.image = self.images_right[self.index]
+                if self.direction == -1:
+                    self.image = self.images_left[self.index]
+
+            # Добавляем графитацию персонажу =)
+            self.vel_y += 1
+            if self.vel_y > 10:
+                self.vel_y = 10
+            dy += self.vel_y
+
+            # Проверяем столкновение
+            for tile in world.tile_list:
+                if tile[1].colliderect(self.rect.x + dx, self.rect.y , self.width, self.height):
+                    dx = 0
+                if tile[1].colliderect(self.rect.x, self.rect.y + dy, self.width, self.height):
+                    if self.vel_y < 0:
+                        dy = tile[1].bottom - self.rect.top
+                        self.vel_y = 0
+                    elif self.vel_y >= 0:
+                        dy = tile[1].top - self.rect.bottom
+                        self.vel_y = 0
+            #  ПРОВЕРКА НА КОСАНИЕ С МАБАИМИ
+            if pygame.sprite.spritecollide(self, blob_group, False):
+                game_over = -1
+            if pygame.sprite.spritecollide(self, lava_group, False):
+                game_over = -1
+            if pygame.sprite.spritecollide(self, door_group, False):
+                game_over = 1
+
+            # обновляем координаты
+            self.rect.x += dx
+            self.rect.y += dy
+
+        elif game_over == -1:
+            self.image = self.dead_image
+            if self.rect.y > 150:
+                self.rect.y -= 5
 
 
-        # Добавляем графитацию персонажу =)
-        self.vel_y += 1
-        if self.vel_y > 10:
-            self.vel_y  = 10
 
-
-        dy += self.vel_y
-
-        # Проверяем столкновение
-
-        # обновляем координаты
-        self.rect.x += dx
-        self.rect.y += dy
-
-        if self.rect.bottom > screen_heigtht:
-            self.rect.bottom = screen_heigtht
-
-
-
-        # РИсует игра в окне
+        #Рисует игру в окне
         screen.blit(self.image, self.rect)
+        pygame.draw.rect(screen, (255, 255, 255), self.rect, 1)
 
-
+        return game_over
 
 class World():
     def __init__(self, data):
         self.tile_list = []
 
         # скачиваем изображения
-        dirt_img = pygame.image.load('dirtCenter.png')
+        dirt_img = pygame.image.load('chocoCenter.png')
         grass_img = pygame.image.load('grassMid.png')
+        grass2_img = pygame.image.load('grassHalfMid.png')
+        lvl2_img = pygame.image.load('castle.png')
 
         row_count = 0
         for row in data:
@@ -96,12 +152,71 @@ class World():
                     img_rect.y = row_count * tile_size
                     tile = (img, img_rect)
                     self.tile_list.append(tile)
+                if tile == 4:
+                    img = pygame.transform.scale(lvl2_img, (tile_size, tile_size))
+                    img_rect = img.get_rect()
+                    img_rect.x = col_count * tile_size
+                    img_rect.y = row_count * tile_size
+                    tile = (img, img_rect)
+                    self.tile_list.append(tile)
+                if tile == 3:
+                    blob = Enemy(col_count * tile_size, row_count * tile_size + 22)
+                    blob_group.add(blob)
+                if tile == 6:
+                    lava = Lava(col_count * tile_size, row_count * tile_size + 25)
+                    lava_group.add(lava)
+                if tile == 8:
+                    door = Door(col_count * tile_size, row_count * tile_size)
+                    door_group.add(door)
+                if tile == 16:
+                    img = pygame.transform.scale(grass2_img, (tile_size, tile_size))
+                    img_rect = img.get_rect()
+                    img_rect.x = col_count * tile_size
+                    img_rect.y = row_count * tile_size
+                    tile = (img, img_rect)
+                    self.tile_list.append(tile)
                 col_count += 1
             row_count += 1
 
     def draw(self):
         for tile in self.tile_list:
             screen.blit( tile[0], tile[1])
+            pygame.draw.rect(screen, (255, 255, 255), tile[1], 1)
+
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load('slimeWalk1.png')
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.move_direction = 1
+        self.move_counter = 0
+
+    def update(self):
+        self.rect.x += self.move_direction
+        self.move_counter += 1
+        if abs(self.move_counter) > 50:
+            self.move_direction *= -1
+            self.move_counter *= -1
+
+class Lava(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        img = pygame.image.load('liquidLavaTop_mid.png')
+        self.image = pygame.transform.scale(img, (tile_size, tile_size // 2))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+
+class Door(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        img = pygame.image.load('doorOpen.png')
+        self.image = pygame.transform.scale(img, (tile_size, tile_size))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
 
 
 
@@ -111,35 +226,49 @@ world_data = [
 [1, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8, 1],
 [1, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 2, 2, 1],
 [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 0, 7, 0, 5, 0, 0, 0, 1],
-[1, 0, 0, 0, 0, 0, 0, 0, 5, 0, 0, 0, 2, 2, 0, 0, 0, 0, 0, 1],
-[1, 7, 0, 0, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+[1, 0, 0, 0, 0, 0, 0, 0, 5, 0, 0, 0, 2, 2, 6, 6, 6, 6, 6, 1],
+[1, 7, 0, 0, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2, 1],
 [1, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
 [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 0, 0, 7, 0, 0, 0, 0, 1],
-[1, 0, 2, 0, 0, 7, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-[1, 0, 0, 2, 0, 0, 4, 0, 0, 0, 0, 3, 0, 0, 3, 0, 0, 0, 0, 1],
-[1, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 1],
-[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+[1, 0, 4, 0, 0, 7, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+[1, 0, 0, 4, 0, 0, 4, 0, 0, 0, 0, 3, 0, 0, 3, 0, 0, 0, 0, 1],
+[1, 0, 0, 0, 4, 0, 0, 0, 0, 4, 4, 4, 4, 4, 4, 4, 0, 0, 0, 1],
+[1, 0, 0, 0, 0, 16, 16, 16, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
 [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 0, 7, 0, 0, 0, 0, 2, 0, 1],
 [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 2, 0, 2, 2, 2, 2, 2, 1],
-[1, 0, 0, 0, 0, 0, 2, 2, 2, 6, 6, 6, 6, 6, 1, 1, 1, 1, 1, 1],
-[1, 0, 0, 0, 0, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-[1, 0, 0, 0, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 0, 1],
+[1, 0, 0, 0, 0, 0, 0, 2, 2, 0, 0, 0, 0, 0, 1, 1, 1, 1, 6, 1],
+[1, 0, 0, 0, 0, 0, 1, 1, 1, 0, 2, 0, 2, 0, 1, 1, 1, 1, 1, 1],
+[1, 0, 0, 0, 0, 1, 1, 1, 1, 6, 6, 6, 6, 6, 1, 1, 1, 1, 1, 1],
 [1, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 ]
 
-player = Player(100, screen_heigtht - 130)
-world = World(world_data)
+player = Player(100, screen_height - 130)
 
+blob_group = pygame.sprite.Group()
+lava_group = pygame.sprite.Group()
+door_group = pygame.sprite.Group()
+
+world = World(world_data)
 
 run = True
 while run:
+
+    clock.tick(fps)
+
     screen.blit(bg_img, (0, 0))
 
     world.draw()
-    player.update()
 
-    draw_grid()
+    if game_over == 0:
+        blob_group.update()
+
+    lava_group.draw(screen)
+    blob_group.draw(screen)
+    door_group.draw(screen)
+
+    game_over = player.update(game_over)
+
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
